ed20739fcdcfbd875c5ffe636d42417310ca1652
spshllp <ceo@sudurpaschim.in>
2025-07-20 05:26:23 +0530

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,13 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      external: []
+    }
+  },
   optimizeDeps: {
     exclude: ['lucide-react'],
+    include: ['@react-three/fiber', '@react-three/drei', '@react-three/xr', 'three', 'react-colorful', 'html2canvas']
   },
 });
